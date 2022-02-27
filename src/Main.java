fd3b681beab2e39fa5e85fbcbab102371b3d422f
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2022-02-26 21:50:10 -0400

    Cambios en main.java

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main{
     public static void main(String[] args) {
         //showMenu();
         Doctor myDoctor = new Doctor("Gabriel Gonzales","Dermatologia");
-        myDoctor.addavailableAppointment(new Date(),"4pm");
 
         Patient patient1 = new Patient("Alejandra","alejandra@gmail.com");
-        //System.out.println(patient1.name);
-        //System.out.println(patient1.email);
+
 
 
     }
