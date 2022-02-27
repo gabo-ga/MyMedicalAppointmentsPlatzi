d2e894b2def04f8483de5b56dcd0c6a8671527d8
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2022-02-27 10:17:34 -0400

    Arraylists en clase doctor para las citas

--- src/Doctor.java
+++ src/Doctor.java
@@ -4,7 +4,9 @@ public class Doctor {
     //atributos
     static int id=0;//autoincrement
     private String name;
+    private String email;
     private String speciality;
+
     //comportamientos
     Doctor(String name,String speciality){
         this.name = name;
@@ -17,4 +19,22 @@ public class Doctor {
         System.out.println("ID Doctor:" +id);
     }
 
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new AvailableAppointment(date,time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment{
+        private int id_availableAppointment;
+        private Date date;
+        private String time;
+        public AvailableAppointment(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main{
     public static void main(String[] args) {
         //showMenu();
         Doctor myDoctor = new Doctor("Gabriel Gonzales","Dermatologia");
-
         Patient patient1 = new Patient("Alejandra","alejandra@gmail.com");
 
-        
+        myDoctor.addAvailableAppointment(new Date(),"6pm");
+        myDoctor.addAvailableAppointment(new Date(),"8pm");
 
+        System.out.println(myDoctor.getAvailableAppointments());
     }
 
 }
